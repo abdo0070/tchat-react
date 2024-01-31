e6424a03a717144069fa360b43e1ff8895c2a7f3
Abdalla <abdallammdouh6@gmail.com>
2024-01-31 15:00:07 +0200

    edit in input message in chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -6,10 +6,9 @@ const Chat = () => {
     <div className="h-full flex flex-col">
       <Header />
       <Messages />
-      <div className="">
-        <form action="">
-          <input className="outline-none w-full p-3 text-lg font-bold"  placeholder="Type..." type="text" />
-        </form>
+      <div className="flex bg-slate-100 justify-between p-3">
+          <input className="outline-none bg-transparent w-full text-lg font-bold"  placeholder="Type..." type="text" />
+          <button className="p-2 w-28 text-white bg-cyan-500 mr-6">SEND</button>
       </div>
     </div>
   )
